ca474b1eb251146a15957d1bc282487d3e7dad0f
Nutdanai Sukkaew <nutdanai.suk@pantavanij.com>
2020-08-21 16:05:10 +0700

    Commit firebase

--- App.js
+++ App.js
@@ -3,12 +3,28 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './Login';
 import Main from './Main';
+import ChallengeRunnder from './ChallengeRunner';
+
+import * as firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAwfuINQBrjzs07LyvEKs1PKthEURPGfp8",
+  authDomain: "social-this-fitting-d30fb.firebaseapp.com",
+  databaseURL: "https://social-this-fitting-d30fb.firebaseio.com",
+  projectId: "social-this-fitting-d30fb",
+  storageBucket: "social-this-fitting-d30fb.appspot.com",
+  messagingSenderId: "522856523662",
+  appId: "1:522856523662:web:b96078f3ad1c1d1c02e6df"
+}
+
+firebase.initializeApp(firebaseConfig)
 
 export default function App() {
   return (
     <View style={styles.container}>
       {/* <Text>Hello, Social This Fitting!</Text> */}
       <StatusBar style="auto" />
+      <ChallengeRunnder />
       <Main />
     </View>
   );
---
+++ ChallengeRunner.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+import * as Constant from './Constant';
+import * as firebase from 'firebase'
+
+import 'firebase/firestore';
+
+export default class ChallengeRunner extends React.Component {
+  state={
+    email:"",
+    password:""
+  }
+
+  componentDidMount() {
+    const dbh = firebase.firestore();
+    dbh.collection("UserChallenge").doc("32465465").set({score: 50000});
+  }
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <Image
+          style={styles.logoImg}
+          source={require('./assets/logo.png')}
+        />
+        <View style={styles.inputView} >
+          <TextInput  
+            style={styles.inputText}
+            placeholder="Email" 
+            placeholderTextColor="#003f5c"
+            onChangeText={text => this.setState({email:text})}/>
+        </View>
+        <View style={styles.inputView} >
+          <TextInput  
+            secureTextEntry
+            style={styles.inputText}
+            placeholder="Password" 
+            placeholderTextColor="#003f5c"
+            onChangeText={text => this.setState({password:text})}/>
+        </View>
+        <FontAwesome.Button name="facebook" style={styles.loginBtn} onClick={() => this.handleLoginFacebook()}>
+          <Text style={styles.loginText}>Login with Facebook</Text>
+        </FontAwesome.Button>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: '100%',
+    backgroundColor: Constant.COLOR_GREY,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo:{
+    fontWeight:"bold",
+    fontSize:28,
+    color:Constant.COLOR_RED,
+    marginBottom:40
+  },
+  logoImg: {
+    width: 150,
+    height: 150,
+    marginBottom:40
+  },
+  inputView:{
+    width:"80%",
+    backgroundColor:Constant.COLOR_WHITE,
+    borderRadius:5,
+    height:50,
+    marginBottom:20,
+    justifyContent:"center",
+    padding:20,
+    display: 'none'
+  },
+  inputText:{
+    height:50,
+    color:Constant.COLOR_GREY,
+  },
+  forgot:{
+    color:Constant.COLOR_GREY,
+    fontSize:11
+  },
+  loginBtn:{
+    backgroundColor:Constant.COLOR_FACEBOOK,
+    borderRadius:5,
+    alignItems:"center",
+    justifyContent:"center",
+    padding: 15
+  },
+  loginText:{
+    color:"white",
+    fontWeight: '700',
+    paddingLeft: 20,
+    paddingRight: 20
+  }
+});
